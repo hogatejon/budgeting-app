29d7280f068d8737e4542456378f7b24be663455
Jonathan Hogate <hogatejon@gmail.com>
2022-09-01 00:00:28 -0500

    Initial commit - add nx / first angular app

---
+++ apps/budget-star/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'budget-smudget-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'budget-star';
+}
