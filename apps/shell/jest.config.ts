5749bbc85d8ece5da04d8e6e71448ee6699d93fd
Jonathan Hogate <hogatejon@gmail.com>
2022-09-01 00:06:16 -0500

    use nx g move to rename to shell

--- apps/shell/jest.config.ts
+++ apps/shell/jest.config.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
 export default {
-  displayName: 'budget-star',
+  displayName: 'shell',
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
@@ -9,7 +9,7 @@ export default {
       stringifyContentPathRegex: '\\.(html|svg)$',
     },
   },
-  coverageDirectory: '../../coverage/apps/budget-star',
+  coverageDirectory: '../../coverage/apps/shell',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
   },
