2bdd12efe78955015bf5c905b4f3384b5c5c89c6
Jonathan Hogate <hogatejon@gmail.com>
2022-09-04 12:41:53 -0500

    add ngrx to shell

--- apps/shell/src/app/app.module.ts
+++ apps/shell/src/app/app.module.ts
@@ -10,11 +10,14 @@ import { TransactionsFeatureTransactionsModule } from '@budget-smudget/transacti
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routing.module';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     AppRoutingModule,
     BrowserModule,
@@ -24,7 +27,20 @@ import { AppRoutingModule } from './app.routing.module';
     TrackersFeatureTrackersModule,
     TransactionsFeatureTransactionsModule,
     DashboardFeatureDashboardModule,
-    RouterModule
+    RouterModule,
+    StoreModule.forRoot(
+      {},
+      {
+        metaReducers: !environment.production ? [] : [],
+        runtimeChecks: {
+          strictActionImmutability: true,
+          strictStateImmutability: true,
+        },
+      }
+    ),
+    EffectsModule.forRoot([]),
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
+    StoreRouterConnectingModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent],
